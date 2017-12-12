cbb7c50f25abf1cd96b43b9a14583fe4df5d880f
mdrab <mateusz.drab@dfg.com>
2017-12-12 15:39:08 +0100

    page object pattern

---
+++ SeleniumWebDriver/SeleniumWebDriverUITests/InvoiceNinjaTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumWebDriverUITests.Pages;
+
+namespace SeleniumWebDriverUITests
+{
+    [TestClass]
+    public class InvoiceNinjaTests
+    {
+        private IWebDriver _driver;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void LoginPageShouldShowDashboardAfterEnteringCredentials()
+        {
+            //var loginPage = new LoginPage(_driver);
+            _driver.Navigate().GoToUrl("http://79.137.68.21/login");
+            IWebElement loginInput = _driver.FindElement(By.Id("email"));
+            IWebElement passwordInput = _driver.FindElement(By.Id("password"));
+            IWebElement submitButton = _driver.FindElement(By.Id("loginButton"));
+
+            loginInput.SendKeys("test1@geekacademy.test");
+            passwordInput.SendKeys("5143testX");
+            submitButton.Click();
+
+            string pageTitle = _driver.Title;
+
+            Assert.AreEqual("Dashboard | Invoice Ninja", pageTitle);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestCleanup]
+        public void End()
+        {
+            _driver.Quit();
+        }
+    }
+}
---
+++ SeleniumWebDriver/SeleniumWebDriverUITests/Pages/LoginPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SeleniumWebDriverUITests.Pages
+{
+    public class LoginPage
+    {
+        [FindsBy(How=How.Id, Using="email")]
+        private IWebElement _loginInput;
+
+        [FindsBy(How=How.Id, Using="password")]
+        private IWebElement _passwordInput;
+
+        [FindsBy(How=How.Id, Using="loginButton")]
+        private IWebElement _submitButton;
+
+        private IWebDriver _driver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            _driver = webDriver;
+            PageFactory.InitElements(_driver, this);
+        }
+    }
+}
